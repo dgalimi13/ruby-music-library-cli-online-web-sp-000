42e6555e40d88dfd3fc308fb44841fda624b4f6e
danny galimi <dannygalimi13@aim.com>
2021-02-14 22:19:09 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -73,7 +73,7 @@ end
       list = Song.all.sort_by {|song| song.name}
       puts "Playing #{list[input-1].name} by #{list[input-1].artist.name}"
     end
-    if input-1 < 1 || input > list.count 
+    if input-1 < 1 || input > list.size
       return nil
     end
   end 
